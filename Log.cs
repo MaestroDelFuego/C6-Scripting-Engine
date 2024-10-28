8b3ffe963d829edb988939cc828e27216ed50471
MaestroDelFuego <160979076+MaestroDelFuego@users.noreply.github.com>
2024-10-28 16:24:34 +0000

    Add files via upload

---
+++ Log.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+
+namespace Test_UI
+{
+    internal static class Log
+    {
+        // Define a delegate and event for logging messages
+        public delegate void LogEventHandler(string message, Color color);
+        public static event LogEventHandler OnLog;
+
+        public static void Error(string message)
+        {
+            RaiseLogEvent($"Error: {message}", Color.Red);
+        }
+
+        public static void Warning(string message)
+        {
+            RaiseLogEvent($"Warning: {message}", Color.Orange);
+        }
+
+        public static void Message(string message)
+        {
+            RaiseLogEvent($"Message: {message}", Color.Green);
+        }
+
+        // Raise the event with the message and specified color
+        private static void RaiseLogEvent(string message, Color color)
+        {
+            OnLog?.Invoke($"[{DateTime.Now:HH:mm:ss}] {message}", color);
+        }
+    }
+}
